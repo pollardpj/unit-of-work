6b1962139f90260238fd71700adbb54c0d0335d1
Phil Pollard <phil@mpgen.co.uk>
2024-12-23 14:23:44 +0000

    simplify using

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -19,37 +19,35 @@ var serviceProvider = services.BuildServiceProvider();
 var context = serviceProvider.GetService<MyAppContext>();
 context.Database.EnsureCreated();
 
-using (var unitOfWork = serviceProvider.GetService<IMyAppUnitOfWork>())
+var unitOfWork = serviceProvider.GetService<IMyAppUnitOfWork>();
+
+var user = new User { Name = "Phil", Email = "phil@pollard.co.uk" };
+await unitOfWork.UserRepository.AddAsync(user);
+
+// Create an Order tied to the user
+var order = new Order { ProductName = "Product #1", Price = 1M, User = user };
+await unitOfWork.OrderRepository.AddAsync(order);
+
+var existingUser = await unitOfWork.UserRepository.GetByIdAsync(4);
+existingUser.Name = "Billy";
+unitOfWork.UserRepository.Update(existingUser);
+
+// Save both User and Order in a single transaction
+await unitOfWork.FlushAsync();
+
+unitOfWork = serviceProvider.GetService<IMyAppUnitOfWork>();
+
+var myUser = await unitOfWork.UserRepository.GetUserWithOrders(4);
+var myUserDto = new UserDto
 {
-    var user = new User { Name = "Phil", Email = "phil@pollard.co.uk" };
-    await unitOfWork.UserRepository.AddAsync(user);
-
-    // Create an Order tied to the user
-    var order = new Order { ProductName = "Product #1", Price = 1M, User = user };
-    await unitOfWork.OrderRepository.AddAsync(order);
-
-    var existingUser = await unitOfWork.UserRepository.GetByIdAsync(4);
-    existingUser.Name = "Billy";
-    unitOfWork.UserRepository.Update(existingUser);
-
-    // Save both User and Order in a single transaction
-    await unitOfWork.FlushAsync();
-}
-
-using (var unitOfWork = serviceProvider.GetService<IMyAppUnitOfWork>())
-{
-    var myUser = await unitOfWork.UserRepository.GetUserWithOrders(4);
-    var myUserDto = new UserDto
+    Id = myUser.Id,
+    Name = myUser.Name,
+    Email = myUser.Email,
+    Orders = myUser.Orders.Select(o => new OrderDto
     {
-        Id = myUser.Id,
-        Name = myUser.Name,
-        Email = myUser.Email,
-        Orders = myUser.Orders.Select(o => new OrderDto
-        {
-            ProductName = o.ProductName,
-            Price = o.Price
-        })
-    };
-    
-    Console.WriteLine(JsonSerializer.Serialize(myUserDto));
-}
+        ProductName = o.ProductName,
+        Price = o.Price
+    })
+};
+
+Console.WriteLine(JsonSerializer.Serialize(myUserDto));
