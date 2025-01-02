0b17ff0e1c3aefbf05d7a85d5108040e6790a56a
Phil Pollard <phil@mpgen.co.uk>
2025-01-02 17:29:51 +0000

    refactor program.cs

---
+++ src/MyAppAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Asp.Versioning;
+using Dapr.Actors;
+using Domain.Actors;
+using Domain.Commands;
+using Domain.Commands.Handlers;
+using Domain.Queries;
+using Domain.Queries.Handlers;
+using Domain.Services;
+using Domain.UnitOfWork;
+using FluentValidation;
+using MyAppAPI.Models;
+using MyAppAPI.Models.Validators;
+using Repository;
+using Shared.CQRS;
+using SharpGrip.FluentValidation.AutoValidation.Endpoints.Extensions;
+using System.Reflection;
+
+namespace MyAppAPI.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMyAppServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IMyAppUnitOfWorkFactory>(sp =>
+        {
+            return new MyAppUnitOfWorkFactory(
+                "Server=(localdb)\\MSSQLLocalDB;Database=myapp",
+                sp.GetService<ILoggerFactory>());
+        });
+
+        services.AddApiVersioning(options =>
+        {
+            options.ApiVersionReader = new UrlSegmentApiVersionReader();
+        });
+
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services
+            .AddScoped<IValidator<OrderRequest>, OrderRequestValidator>()
+            .AddScoped<IValidator<GetOrdersRequest>, GetOrdersRequestValidator>()
+            .AddFluentValidationAutoValidation();
+
+        services
+            .AddScoped<ICommandHandler<CreateOrder>, CreateOrderHandler>()
+            .AddScoped<IOrderEventsService, OrderEventsService>();
+
+        services
+            .AddScoped<IQueryHandler<GetOrders, GetOrdersResult>, GetOrdersHandler>();
+
+        services.AddActors(options =>
+        {
+            options.Actors.RegisterActor<OrderActor>();
+
+            options.ActorIdleTimeout = TimeSpan.FromSeconds(5);
+
+            options.ReentrancyConfig = new ActorReentrancyConfig
+            {
+                Enabled = false
+            };
+        });
+
+        return services;
+    }
+}
---
+++ src/MyAppAPI/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Asp.Versioning;
+using AutoMapper;
+using Domain.Commands;
+using Domain.Exceptions;
+using Domain.Queries;
+using MyAppAPI.Models;
+using Shared.CQRS;
+using SharpGrip.FluentValidation.AutoValidation.Endpoints.Extensions;
+
+namespace MyAppAPI.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication AddMyAppMiddleware(this WebApplication app)
+    {
+        var versionSet = app.NewApiVersionSet()
+            .HasApiVersion(new ApiVersion(1))
+            .HasApiVersion(new ApiVersion(2))
+            .Build();
+
+        // Dapr will send serialized event object vs. being raw CloudEvent
+        app.UseCloudEvents();
+
+        app.MapPost("/api/{version:apiVersion}/order",
+            async (
+                OrderRequest request,
+                ICommandHandler<CreateOrder> handler,
+                IMapper mapper) =>
+            {
+                var command = mapper.Map<CreateOrder>(request);
+
+                await handler.ExecuteAsync(command);
+
+                return Results.Ok(new
+                {
+                    OrderReference = request.Reference,
+                    OrderPrice = command.Price
+                });
+
+            })
+            .AddFluentValidationAutoValidation()
+            .WithApiVersionSet(versionSet)
+            .MapToApiVersion(1);
+
+        app.MapGet("/api/{version:apiVersion}/orders",
+            async (
+                [AsParameters] GetOrdersRequest request,
+                IQueryHandler<GetOrders, GetOrdersResult> handler,
+                IMapper mapper) =>
+            {
+                try
+                {
+                    return Results.Ok(
+                        await handler.ExecuteAsync(mapper.Map<GetOrders>(request)));
+                }
+                catch (PagedQueryException ex)
+                {
+                    return Results.BadRequest(ex.Details);
+                }
+
+            })
+            .AddFluentValidationAutoValidation()
+            .WithApiVersionSet(versionSet)
+            .MapToApiVersion(1);
+
+        app.MapActorsHandlers();
+
+        return app;
+    }
+}
