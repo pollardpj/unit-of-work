4af2858b781034cf3bb70bb903ae140cd1f45086
Phil Pollard <phil@mpgen.co.uk>
2025-01-08 09:49:16 +0000

    add migrations

---
+++ src/Migrations/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+Console.WriteLine("Migrations Application Initialised");
+
+public class MyAppContextFactory : IDesignTimeDbContextFactory<MyAppContext>
+{
+    public MyAppContext CreateDbContext(string[] args)
+    {
+        var connectionString = args.Length > 0 ? args[0] : "Server=(localdb)\\MSSQLLocalDB;Database=myapp_migrations";
+
+        var options = new DbContextOptionsBuilder<MyAppContext>()
+            .UseSqlServer(connectionString, b => b.MigrationsAssembly("Migrations"))
+            .Options;
+
+        return new MyAppContext(options);
+    }
+}
